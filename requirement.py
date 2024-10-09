0e9c83a158b2f99c6b2416fdfee88960c49a8672
Sudhan Shrestha <sudhanbshrestha@gmail.com>
2024-10-09 16:24:18 -0400

    Recreating the project from start

---
+++ requirement.py
@@ -0,0 +1,19 @@
+# requirements.py
+import subprocess
+import sys
+
+# List of required libraries for the project
+required_libraries = [
+    'pandas',        
+    'numpy',         
+    'scikit-learn',  
+    'matplotlib',     
+    'seaborn',          
+    'joblib'         
+]
+
+for library in required_libraries:
+    try:
+        __import__(library)
+    except ImportError:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', library])
